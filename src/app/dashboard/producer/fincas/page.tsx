8c0b2ecdf0b82764544f895b308e4625e9ce0431
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2024-12-04 22:45:40 -0500

    feat(business-table): implement merchant business table functionality

--- src/app/dashboard/producer/fincas/page.tsx
+++ src/app/dashboard/producer/fincas/page.tsx
@@ -1,9 +1,53 @@
 'use client';
-import React from 'react';
+import { Box, Center } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import TableBusiness from '../../../../components/business/table-business/TableBusiness';
+import TableExport from '../../../../components/export/table-export/TableExport';
 import IsOnboarding from '../../../../components/ui/IsOnboarding';
 
+interface WindowSizeProps {
+  width: number | null;
+  height: number | null;
+}
+
 function FincasPage() {
-  return <div>FincasPage</div>;
+  const [windowSize, setWindowSize] = useState<WindowSizeProps>({
+    width: null,
+    height: null,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <Box
+      my={'auto'}
+      mx={'auto'}
+      width={{
+        sm: `${Number(windowSize.width) - 20}px`,
+        md: `${Number(windowSize.width) - 300}px`,
+      }}
+    >
+      <Center mt={'30px'}>
+        <TableBusiness
+          windowSize={windowSize}
+          width={{
+            sm: Number(windowSize.width) - 20,
+            md: Number(windowSize.width) - 300,
+          }}
+        />
+      </Center>
+    </Box>
+  );
 }
 
 export default IsOnboarding(FincasPage);
