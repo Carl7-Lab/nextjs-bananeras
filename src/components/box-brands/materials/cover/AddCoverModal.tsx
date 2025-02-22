47cda334ee8ab630e142f5f8489e2a2fe97f88b4
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-02-21 21:20:59 -0500

    feat(boxBrand-cover): implement UI for cover material management

---
+++ src/components/box-brands/materials/cover/AddCoverModal.tsx
@@ -0,0 +1,44 @@
+import {
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
+import { AiFillPlusCircle } from 'react-icons/ai';
+import AddCoverForm from './AddCoverForm';
+
+const AddCoverModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <IconButton
+        position='relative'
+        top='-5px'
+        ml='5px'
+        isRound
+        aria-label='agregar cubierta'
+        size='base'
+        colorScheme='teal'
+        variant='elevated'
+        icon={<AiFillPlusCircle size='20px' />}
+        onClick={onOpen}
+      />
+      <Modal isOpen={isOpen} onClose={onClose} size='md'>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <AddCoverForm onClose={onClose} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AddCoverModal;
---
+++ src/hooks/box-brand/materials/cover/createCover.ts
@@ -0,0 +1,27 @@
+import { useMutation } from 'react-query';
+import axios from '@/lib/axios';
+import { MutationConfig } from '@/lib/react-query';
+import { CoverType } from '@/types/box-brand/materials/cover';
+
+interface CreateCoverResponse {
+  coverId: string;
+}
+
+export const createCover = (
+  data: Partial<CoverType>
+): Promise<CreateCoverResponse> => {
+  return axios.post('/box-brand/cover', data);
+};
+
+type UseCreateCoverOptions = {
+  config?: MutationConfig<typeof createCover>;
+};
+
+export const useCreateCover = ({ config }: UseCreateCoverOptions = {}) => {
+  const mutation = useMutation({
+    ...config,
+    mutationFn: createCover,
+  });
+
+  return { ...mutation, createCover: mutation.mutate };
+};
