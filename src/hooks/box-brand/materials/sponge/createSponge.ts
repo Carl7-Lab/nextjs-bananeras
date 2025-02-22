87cc6b33224fb87e4fb4f2cdff99abf2e05c2032
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-02-21 21:24:08 -0500

    feat(boxBrand-sponge): implement UI for sponge material management

---
+++ src/hooks/box-brand/materials/sponge/createSponge.ts
@@ -0,0 +1,27 @@
+import { useMutation } from 'react-query';
+import axios from '@/lib/axios';
+import { MutationConfig } from '@/lib/react-query';
+import { SpongeType } from '@/types/box-brand/materials/sponge';
+
+interface CreateSpongeResponse {
+  spongeId: string;
+}
+
+export const createSponge = (
+  data: Partial<SpongeType>
+): Promise<CreateSpongeResponse> => {
+  return axios.post('/box-brand/sponge', data);
+};
+
+type UseCreateSpongeOptions = {
+  config?: MutationConfig<typeof createSponge>;
+};
+
+export const useCreateSponge = ({ config }: UseCreateSpongeOptions = {}) => {
+  const mutation = useMutation({
+    ...config,
+    mutationFn: createSponge,
+  });
+
+  return { ...mutation, createSponge: mutation.mutate };
+};
